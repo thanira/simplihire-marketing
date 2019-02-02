a66b7a2be1de5b6e0e8737a348238183f7d2ad61
thanira <Thanira Rates>
2019-02-02 21:42:04 +0000

    fixed

--- SimpliHire_Site/js/main.js
+++ SimpliHire_Site/js/main.js
@@ -134,41 +134,4 @@ jQuery(document).ready(function ($) {
     }
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
-  });
-
-  // Clients carousel (uses the Owl Carousel library)
-  $(".clients-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 2
-      },
-      768: {
-        items: 4
-      },
-      900: {
-        items: 6
-      }
-    }
-  });
-
-
 });
